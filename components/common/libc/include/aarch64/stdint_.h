963045f0eb444547ed0facf35f3f3145a0625450
Imre Kis <imre.kis@arm.com>
2024-05-29 14:18:58 +0200

    Remove int128_t
    ISO C does not support int128_t. Remove type to prevent compiler warnings.
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: I0ec47f265d04f700954701748291318aa95a2f97

--- components/common/libc/include/aarch64/stdint_.h
+++ components/common/libc/include/aarch64/stdint_.h
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 /*
- * Portions copyright (c) 2020, Arm Limited and Contributors.
+ * Portions copyright (c) 2020-2024, Arm Limited and Contributors.
  * All rights reserved.
  */
 
@@ -24,8 +24,5 @@ typedef long int64_least_t;
 typedef unsigned long uint64_least_t;
 typedef long int64_fast_t;
 typedef unsigned long uint64_fast_t;
-
-typedef __int128 int128_t;
-typedef unsigned __int128 uint128_t;
 
 #endif
